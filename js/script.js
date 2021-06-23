cc4fbf638f818d9f366112736b5bb6313c22e174
Кириллова Наталья <natalka.kirillov@mail.ru>
2021-06-23 20:11:32 +0300

    создание объектов с ключами

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
 let hello;
 hello='Hello my World!!!';
 console.log(hello);
+
+
+let question1 {
+	question: 'What is the capital of Russia?',
+	answer: 'Moscow',
+	currectAnswer: 'Moscow'
+}
+
+let question2 {
+	question: 'Соль это?',
+	answer: 'приправа',
+	currectAnswer: 'белая смерть'
+}
+
+let question3 {
+	question: 'Сахар это?',
+	answer: 'приправа',
+	currectAnswer: 'сладкая смерть'
+}
+
+let question4 {
+	question: 'Колбаса это?',
+	answer: 'на бутерброд',
+	currectAnswer: 'вкусная смерть'
+}

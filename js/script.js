ad4dc0d581e8d0e3e623cae9d41dc1c7a2c1bf6d
Кириллова Наталья <natalka.kirillov@mail.ru>
2021-06-30 16:20:05 +0300

    массивы, методы массивов 4ур

--- js/script.js
+++ js/script.js
@@ -2,69 +2,44 @@ let question1 = {
   question: 'What is the capital of Russia?',
   answer: 'Moscow'
 }
-question1.currectAnswer = 'Moscow';
+question1.correctAnswer = 'Moscow';
 question1.answer=['Moscow','Minsk','New York'];
-console.log(question1);
 
 
 let question2 = {
-  question: 'Имя писателя Пушкина?',
-  answer: 'Александр',
-  currectAnswer: 'c'
+  question: 'Третья по счету буква в англ алфавите',
+  answer: 'c',
+  correctAnswer: 'c'
 }
-question2.answer=['Олег','Андрей'];
-console.log(question2);
+question2.answer=['c','d'];
+
 
 let question3 = {
   question: 'Сколько спутников у Земли?',
   answer: '1',
-  currectAnswer: '1'
+  correctAnswer: '1'
 }
-question3.answer=['2','3','0'];
-console.log(question3);
+question3.answer=['2','1','0'];
+
 
 let question4 = {
   question: 'Как называется спутник Земли?',
   answer: 'Луна',
-  currectAnswer: 'Луна'
+  correctAnswer: 'Луна'
 }
-question4.answer=['Сатурн','Венера'];
-console.log(question4);
+question4.answer=['Сатурн','Луна'];
 
-//преобразование в массив
-let arr1=Object.entries(question1);
-arr1.forEach(([key,value]) =>{
-console.log(key);
-console.log(value);
-});
-let arr2=Object.entries(question2);
-arr2.forEach(([key,value]) =>{
-console.log(key);
-console.log(value);
-});
-let arr3=Object.entries(question3);
-arr3.forEach(([key,value]) =>{
-console.log(key);
-console.log(value);
-});
-let arr4=Object.entries(question4);
-arr4.forEach(([key,value]) =>{
-console.log(key);
-console.log(value);
-});
-
-
-//объединение массивов
-let arr=arr1.concat(arr2,arr3,arr4);
+const arr=[];
+arr.push(question1,question2,question3,question4);
 console.log(arr);
 
-//поиск 
-/*
-arr.forEach((item)=>{
-  if(item.currectAnswer === 'c'){
-    console.log(item);
+arr.forEach((item) => {
+  if(item.correctAnswer==='c'){
+  console.log(item);
   }
-});*/
+});
 
-const otbor=arr.filter((item) => item === 'c');
-console.log(otbor);
+
+
+
+
